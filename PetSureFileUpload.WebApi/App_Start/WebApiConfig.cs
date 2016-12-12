a9b3993518036eb1e72fb463aed325353538882e
Katherina Ann Santos <Katherina Ann Santos@egg.ph>
2016-12-12 10:51:46 +0800

    Initial project setup

---
+++ PetSureFileUpload.WebApi/App_Start/WebApiConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace PetSureFileUpload.WebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.Routes.MapHttpRoute("DefaultApi", "api/{controller}/{action}/{id}", new { id = RouteParameter.Optional }, null);
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
---
+++ PetSureFileUpload.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetSureFileUpload.WebApi.MvcApplication" Language="C#" %>
